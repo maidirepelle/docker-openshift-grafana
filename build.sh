6df5512465b3e6733d6c614987275ec240c9aa1a
Simone P <maidirepelle@gmail.com>
2020-02-12 11:14:56 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export VERSION=5.4.4
-docker build . -t wkulhanek/grafana:latest
-docker tag wkulhanek/grafana:latest wkulhanek/grafana:${VERSION}
-docker push wkulhanek/grafana:${VERSION}
-docker push wkulhanek/grafana:latest
